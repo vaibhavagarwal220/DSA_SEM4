bbe1e3aa2b11f71fbaa9c2877c9f185ab5852f52
vaibhavagarwal220 <vaibhavagarwal220@gmail.com>
2017-05-02 17:18:35 +0530

    added file input for floodfill

--- A7/FloodFill.cpp
+++ A7/FloodFill.cpp
@@ -18,17 +18,11 @@ int main(int argc, char *argv[])
   cout<<"Enter number of columns : ";
   cin>>m;
 
-  cout<<"Enter Source X coordinate : ";
-  cin>>s.first;
-  cout<<"Enter Source Y coordinate : ";
-  cin>>s.second;
-  cout<<"Enter Destination X coordinate : ";
-  cin>>d.first;
-  cout<<"Enter Destination Y coordinate : ";
-  cin>>d.second;
+s=make_pair(0,0);
+d=make_pair(0,0);
+cout<<"Enter the Maze :\n";
 
-
-  char a[n+2][m+2];
+  char a[n+2][m+2],t;
   UndirectedGraph g(n*m , 'l');
   
   for(int i=0;i<m+2;i++)
@@ -42,30 +36,29 @@ int main(int argc, char *argv[])
   for(int i=1;i<=n;i++)
       {
       for(int j=1;j<=m;j++)
-        a[i][j]='O';
+        {
+        	cin>>a[i][j];
+        	//cin>>t;
+        	if(a[i][j]=='S') {s=make_pair(i,j);a[i][j]='O';}
+        	else if(a[i][j]=='D') {d=make_pair(i,j);a[i][j]='O';}
+        }
+        //cin>>t;
       }
 
-
-
-  cout<<"Enter Number of cells which cannot be visited : ";
-  cin>>num_x;
-  for(int i=0;i<num_x;i++)
-      {
-        cout<<"Enter X coordinate["<<i+1<<"] : ";
-        cin>>x.first;
-        cout<<"Enter Y coordinate ["<<i+1<<"] : ";
-        cin>>x.second;
-        if(x.first<=n&&x.first>=1 && x.second<=n&&x.second>=1)
-          a[x.first][x.second]='X';
-      }
-
-         cout<<endl;
-    for(int i=1;i<=n;i++)
+      		cout<<endl;
+            cout<<endl;
+      for(int i=1;i<=n;i++)
       {
       for(int j=1;j<=m;j++)
-           cout<<a[i][j]<<" ";
+           {
+           	if(i==s.first&&j==s.second) cout<<"S ";
+           	else if (i==d.first&&j==d.second) cout<<"D ";
+           	else cout<<a[i][j]<<" ";
+           }
+
          cout<<endl;
       }
+               cout<<endl;
 
       for(int i=n;i>=1;i--)
       {
@@ -78,69 +71,26 @@ int main(int argc, char *argv[])
                 for(int k=sx;k<sx+3;k++)
                   for(int l=sy;l<sy+3;l++)
                     {
-                      if(k%3==1 && l%3 == 1) continue;
+                      //if(k%3==1 && l%3 == 1) continue;
                       int d1=i+dirx[k%3];
                       int d2=j+diry[l%3];
                       if(a[d1][d2]=='O') 
                       {
-                        //if((i-1)*m +(j-1) == (d1 - 1)*m + (d2 - 1))
-                          //continue;
+                        if((i-1)*m +(j-1) == (d1 - 1)*m + (d2 - 1))
+                          continue;
                         g.add( (i-1)*m +(j-1) , (d1 - 1)*m + (d2 - 1) );
                       }
 
                     }
-                /*pair<int,int> left = make_pair(i,j-1);
-                pair<int,int> right = make_pair(i,j+1);
-                pair<int,int> up = make_pair(i-1,j);
-                pair<int,int> down = make_pair(i+1,j);
-                pair<int,int> left_up = make_pair(i-1,j-1);
-                pair<int,int> right_up = make_pair(i-1,j+1);
-                pair<int,int> left_down = make_pair(i+1,j-1);
-                pair<int,int> right_down = make_pair(i+1,j+1);
-
-                if(a[left.first][left.second]=='O') 
-                {
-                    g.add( (i-1)*m +(j-1) , (left.first - 1)*m + (left.second - 1) );
-                }
-                if(a[right.first][right.second]=='O') 
-                {
-                   g.add(  (i-1)*m +(j-1) , (right.first - 1)*m + (right.second - 1));
-                }
-                if(a[up.first][up.second]=='O') 
-                {
-                    g.add( (i-1)*m +(j-1), (up.first - 1)*m + (up.second - 1));
-                }
-                if(a[down.first][down.second]=='O') 
-                {
-                    g.add( (i-1)*m +(j-1),(down.first - 1)*m + (down.second - 1));
-                }
-                if(a[left_up.first][left_up.second]=='O') 
-                {
-                    g.add( (i-1)*m +(j-1) , (left_up.first - 1)*m + (left_up.second - 1));
-                }
-                if(a[right_up.first][right_up.second]=='O') 
-                {
-                    g.add( (i-1)*m +(j-1),(right_up.first - 1)*m + (right_up.second - 1));
-                }
-                if(a[left_down.first][left_down.second]=='O') 
-                {
-                    g.add( (i-1)*m +(j-1),(left_down.first - 1)*m + (left_down.second - 1));
-                }
-                if(a[right_down.first][right_down.second]=='O') 
-                {
-                    g.add( (i-1)*m +(j-1),(right_down.first - 1)*m + (right_down.second - 1));
-                } 
-*/
+        
             } 
           
-//          cout<<endl;
           }
 
-  //       cout<<endl;
       }
       src=(s.first-1)*m + (s.second-1);
       dest=(d.first-1)*m + (d.second-1);
-      g.print();
+      //g.print();
       LinearList<DFSNode> arr(n*m);
       arr=g.dfs(work,dest);
     /*  LinearList< list<int> > predetree(n*m);
@@ -155,18 +105,26 @@ int main(int argc, char *argv[])
 		cout<<endl;
 		} */     	
     int tmp=src;
+    if(arr[tmp].pred==-1) 
+    {
+      cout<<"\n No path found\n";
+      cout<<" END\n\n\n";
+      return 0;
+    }
+
       //cout<<tmp;
       while(tmp!=-1)
       {
         cout<<" ("<<tmp/m + 1 <<","<<tmp%m + 1<<") = >";
-        //cout<<tmp<<endl;
         if(tmp == dest) break;
-        tmp=arr[tmp].pred;
-        if(tmp==-1) {not_found=true;break;}
+        tmp=arr[tmp].pred;  
       }
 
-      if(not_found) cout<<"\n No path found\n";
       cout<<" END\n";
+  
+
+               cout<<endl;
+               cout<<endl;
 
 	return 0;
 }
